15f75fe10621f20db2b365ee8490155e323bd658
vnothkumar <vinothkumar.sagunthala@gmail.com>
2026-01-02 10:15:02 +0530

    chore: Update Astro to v5.16.6 and migrate session driver to Cloudflare KV.

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,10 +1,5 @@
 
 export default new Map([
-["src/content/products/jackal-rifle.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproducts%2Fjackal-rifle.mdx&astroContentModuleFlag=true")],
-["src/content/products/tactical-vest-mk2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproducts%2Ftactical-vest-mk2.mdx&astroContentModuleFlag=true")],
-["src/content/products/combat-knife-elite.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproducts%2Fcombat-knife-elite.mdx&astroContentModuleFlag=true")],
-["src/content/products/precision-rifle-scope.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproducts%2Fprecision-rifle-scope.mdx&astroContentModuleFlag=true")],
-["src/content/products/night-hawk-sniper.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproducts%2Fnight-hawk-sniper.mdx&astroContentModuleFlag=true")],
 ["src/content/products/rifle/jackal-rifle.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproducts%2Frifle%2Fjackal-rifle.mdx&astroContentModuleFlag=true")],
 ["src/content/products/rifle/night-hawk-sniper.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproducts%2Frifle%2Fnight-hawk-sniper.mdx&astroContentModuleFlag=true")],
 ["src/content/products/rifle/precision-rifle-scope.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproducts%2Frifle%2Fprecision-rifle-scope.mdx&astroContentModuleFlag=true")],
